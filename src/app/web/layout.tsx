2e14c473611a70b6be715e9c82c996fa06fa0370
João de Conto <joaovicentedeconto@gmail.com>
2025-08-20 08:42:47 -0300

    Vercel ok

--- src/app/web/layout.tsx
+++ src/app/web/layout.tsx
@@ -2,6 +2,7 @@ import '../../ui/styles/globals.css';
 import '../../ui/styles/tokens.css';
 import Providers from './lib/providers';
 import AppSidebar from '@ui/components/shell/AppSidebar';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata = { title: 'README Studio • Pro' };
 
@@ -15,6 +16,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             <div className="flex-1 flex flex-col">{children}</div>
           </div>
         </Providers>
+        <SpeedInsights />
       </body>
     </html>
   );
