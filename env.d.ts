8c4850d8ba8c031b42a1f16696526ba893fd4607
João de Conto <joaovicentedeconto@gmail.com>
2025-08-20 11:45:31 -0300

    env

--- env.d.ts
+++ env.d.ts
@@ -2,13 +2,13 @@
 
 declare namespace NodeJS {
   interface ProcessEnv {
-    GITHUB_APP_ID?: string;
+    GITHUB_APP_ID?: 1797390;
     GITHUB_APP_CLIENT_ID?: string;
     GITHUB_APP_CLIENT_SECRET?: string;
-    GITHUB_APP_WEBHOOK_SECRET?: string;
+    GITHUB_APP_WEBHOOK_SECRET?: abc123;
     GITHUB_APP_PRIVATE_KEY_B64?: string;
     NEXT_PUBLIC_POSTHOG_KEY?: string;
-    DATABASE_URL?: string;
+    DATABASE_URL?: 'http://localhost:3001';
   }
 }
 
