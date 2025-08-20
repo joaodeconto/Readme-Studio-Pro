e221cbdcccc17b484a30a1dcfff252294f138c4c
João de Conto <joaovicentedeconto@gmail.com>
2025-08-20 06:18:32 -0300

    fix: replace remark-slug with rehype-slug

--- src/ui/components/editor/MdPreview.tsx
+++ src/ui/components/editor/MdPreview.tsx
@@ -1,13 +1,16 @@
 "use client";
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
-import remarkSlug from 'remark-slug';
 import remarkBreaks from 'remark-breaks';
+import rehypeSlug from 'rehype-slug';
 
 export default function MdPreview({ value }: { value: string }) {
   return (
     <div className="prose prose-tight dark:prose-invert max-w-none h-[calc(100dvh-12rem)] overflow-auto border rounded p-4">
-      <ReactMarkdown remarkPlugins={[remarkGfm, remarkSlug, remarkBreaks]}>
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm, remarkBreaks]}
+        rehypePlugins={[rehypeSlug]}
+      >
         {value}
       </ReactMarkdown>
     </div>
