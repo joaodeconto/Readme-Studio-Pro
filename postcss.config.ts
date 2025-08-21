060ce1c93ace025b3fe16f58c33225300bf914f7
João de Conto <joaovicentedeconto@gmail.com>
2025-08-21 15:34:37 -0300

    .ignore

--- postcss.config.ts
+++
@@ -1,9 +0,0 @@
-import tailwindcss from '@tailwindcss/postcss';
-import autoprefixer from 'autoprefixer';
-import type { Plugin } from 'postcss';
-
-const config = {
-  plugins: [tailwindcss, autoprefixer],
-} satisfies { plugins: Plugin[] };
-
-export default config;
--- types.d.ts
+++
@@ -1,3 +0,0 @@
-declare module '@codemirror/lang-markdown';
-declare module 'remark-slug';
-declare module 'diff-match-patch';
